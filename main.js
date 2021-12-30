a7316790ff3605ab6ae24afacae819931700c089
OpAayan <95918550+OpAayan@users.noreply.github.com>
2021-12-30 10:48:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var first_name = "Aayan";
+console.log(first_name);
+
+var last_name = "Khan";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 25;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
+var subtract = a-b;
+console.log(subtract);
+
+var multiply = a * b ;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
+
+
